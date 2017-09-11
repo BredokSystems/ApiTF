b5bf8390fe2977141a684c94c403c645b56c9ab9
Lev_Serba <serba02@gmail.com>
2017-09-11 13:54:46 +0300

    added simple testng tests

---
+++ src/main/java/Pages/Expedia.java
@@ -0,0 +1,6 @@
+package Pages;
+
+public class Expedia {
+    public final String EXPEDIA_HOME_PAGE_URL = "https://www.expedia.com";
+    public final String EXPEDIA_VACATION_RENTALS_URL = "https://www.expedia.com/vacation-rentals";
+}
---
+++ src/test/java/BasicFeaturesTests.java
@@ -0,0 +1,38 @@
+import Pages.Expedia;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class BasicFeaturesTests {
+    Expedia expediaPage = new Expedia();
+    //@Test
+    public void testStatuscode(){
+        given().get(expediaPage.EXPEDIA_HOME_PAGE_URL).
+        then().statusCode(200).contentType(ContentType.HTML);
+    }
+    @Test
+    public void testExtractDetailsUsingResponse(){
+       Response response = when().get(expediaPage.EXPEDIA_HOME_PAGE_URL).
+                           then().extract().response();
+        System.out.println("Content Type: "+response.getContentType());
+        System.out.println("Href: "+response.path("url"));
+        System.out.println("Status code: "+response.statusCode());
+    }
+    @Test
+    public void testTopVacationRentals(){
+        given().get(expediaPage.EXPEDIA_VACATION_RENTALS_URL).
+                then().statusCode(200).log().all();
+        //Response response = when().get(expediaPage.EXPEDIA_VACATION_RENTALS_URL).
+                //then().extract().response();
+
+        //System.out.println("Href: "+response.path("url"));
+       // System.out.println("Status code: "+response.statusCode());
+    }
+
+
+}
